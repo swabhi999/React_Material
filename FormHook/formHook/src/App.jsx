9f8b62a34d40b6d66be453bbadf084fff32e0b65
sawabhi22@gmail.com <sawabhi22@gmail.com>
2025-11-18 13:39:23 +0530

    first commit

---
+++ FormHook/formHook/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import React from 'react'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+<div className="bg-cyan-600 w-screen h-screen  flex justify-center items-center ">
+
+  <h1 className='text-5xl font-bold text-white'>Swabhiman Magar</h1>
+
+</div>
+    </>
+  )
+}
+
+export default App
