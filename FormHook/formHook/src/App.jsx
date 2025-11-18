db8e2be36a257b65d60e9d11ba312ea8e48033a6
sawabhi22@gmail.com <sawabhi22@gmail.com>
2025-11-18 13:51:55 +0530

    changing

--- FormHook/formHook/src/App.jsx
+++ FormHook/formHook/src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from 'react'
+import { useForm } from "react-hook-form"
 import React from 'react'
 import './App.css'
 
 function App() {
+    const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+
+
  
   return (
     <>
 <div className="bg-cyan-600 w-screen h-screen  flex justify-center items-center ">
 
-  <h1 className='text-5xl font-bold text-white'>Swabhiman Magar</h1>
 
 </div>
     </>
